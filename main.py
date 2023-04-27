d0d5d5d062c445e85c62e3d67e81711088687b72
ferferreres <ferferreres98@gmail.com>
2023-04-27 12:05:59 +0200

    prueba1

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from pyodide.http import open_url
 
-df = pd.read_csv('res/Dependencies.csv')
+url = 'https://raw.githubusercontent.com/ferferreres/Pruebas_PyScript/main/res/Dependencies.csv'
+url_content = open_url(url)
 
-csv = Element('csv')
-csv.write(df.shape)
+df = pd.read_csv(url_content)
+print(df['functional_domain'])
